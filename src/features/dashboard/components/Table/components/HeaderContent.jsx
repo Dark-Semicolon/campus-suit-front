5b7216b405e7c429f76650cb20bfda299ec5f535
ahmed <ahmedosama12313@gmail.com>
2024-05-16 21:53:05 +0300

    solve a bug

--- src/features/dashboard/components/Table/components/HeaderContent.jsx
+++ src/features/dashboard/components/Table/components/HeaderContent.jsx
@@ -2,66 +2,40 @@ import { useCallback } from "react";
 import Filter from "@/components/Filter";
 import ClearParamsButton from "@/components/ClearParamsButton";
 import VisibleColumns from "./VisibleColumns";
-import CreateButton from "./CreateButton";
+import CreateButton from "@/components/CreateButton";
+import Search from "@/components/Search";
 import ItemPearPage from "./ItemPearPage";
-import Search from "../../../../../components/Search";
 
-function HeaderContent({
-  filterOptions,
-  visibleColumns,
-  setVisibleColumns,
-  headers,
-  totalRows,
-  rowsNumber,
-  setRowsNumber,
-  addRow,
-  searchValue,
-  setSearchValue,
-  placeholder,
-}) {
-  const onRowsPerPageChange = useCallback(
-    (e) => {
-      setRowsNumber(Number(e.target.value));
-    },
-    [setRowsNumber]
-  );
+function HeaderContent({ filterOptions, visibleColumns, setVisibleColumns, headers, totalRows, rowsNumber, setRowsNumber, addRow, searchValue, setSearchValue, placeholder }) {
+    const onRowsPerPageChange = useCallback(
+        (e) => {
+            setRowsNumber(Number(e.target.value));
+        },
+        [setRowsNumber]
+    );
 
-  return (
-    <div className="flex flex-col gap-4">
-      <div className="flex flex-wrap items-end justify-between gap-3 py-5">
-        {setSearchValue ? (
-          <Search
-            searchValue={searchValue}
-            setSearchValue={setSearchValue}
-            placeholder={placeholder}
-          />
-        ) : null}
-        <div className="flex flex-wrap gap-3">
-          {filterOptions && (
-            <Filter key="filter" filterOptions={filterOptions} />
-          )}
+    return (
+        <div className="flex flex-col gap-4">
+            <div className="flex flex-wrap items-end justify-between gap-3 py-5">
+                {setSearchValue ? <Search searchValue={searchValue} setSearchValue={setSearchValue} placeholder={placeholder} /> : null}
+                <div className="flex flex-wrap gap-3">
+                    {filterOptions && <Filter key="filter" filterOptions={filterOptions} />}
 
-          {visibleColumns && (
-            <VisibleColumns
-              visibleColumns={visibleColumns}
-              setVisibleColumns={setVisibleColumns}
-              headers={headers}
-            />
-          )}
+                    {visibleColumns && <VisibleColumns visibleColumns={visibleColumns} setVisibleColumns={setVisibleColumns} headers={headers} />}
 
-          {filterOptions && <ClearParamsButton />}
+                    {filterOptions && <ClearParamsButton />}
 
-          {addRow && addRow.permission && <CreateButton addRow={addRow} />}
+                    {addRow && addRow.permission && <CreateButton addRow={addRow} />}
+                </div>
+            </div>
+
+            <div className="flex flex-wrap items-center justify-between gap-3">
+                <span className="font-medium text-blue-color-primary text-md">Number of {totalRows || 0} rows</span>
+
+                <ItemPearPage rowsNumber={rowsNumber} onChange={onRowsPerPageChange} />
+            </div>
         </div>
-      </div>
-
-      <div className="flex flex-wrap items-center justify-between gap-3">
-        <span className="font-medium text-blue-color-primary text-md">Number of {totalRows || 0} rows</span>
-
-        <ItemPearPage rowsNumber={rowsNumber} onChange={onRowsPerPageChange} />
-      </div>
-    </div>
-  );
+    );
 }
 
 export default HeaderContent;
