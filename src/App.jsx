318f22e4fb4f86890d56d7d6537d2e76e8c7b725
ahmed <ahmedosama12313@gmail.com>
2024-05-23 08:17:33 +0300

    fix matterial ui error in app file

--- src/App.jsx
+++ src/App.jsx
@@ -5,71 +5,67 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider } from "@emotion/react";
-import { createTheme } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 
 import { Toaster } from "react-hot-toast";
 
 import LoaderPage from "./components/LoaderPage";
 import useRoutes from "./routes/useRoutes";
 
-
 // React Query settings
 const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: 0,
-        },
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
     },
+  },
 });
 
 const theme = createTheme({
-    typography: {
-        fontFamily: "Marhey",
-    },
+  typography: {
+    fontFamily: "Marhey",
+  },
 });
 
 function App() {
+  const routes = useRoutes();
 
-    const routes = useRoutes()
+  return (
+    <ThemeProvider theme={theme}>
+      <NextUIProvider>
+        <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools initialIsOpen={false} />
+          <BrowserRouter>
+            <Suspense fallback={<LoaderPage />}>
+              <Routes>{routes}</Routes>
+            </Suspense>
+          </BrowserRouter>
 
-    return (
-        <ThemeProvider theme={theme}>
-            <NextUIProvider>
-                <QueryClientProvider client={queryClient}>
-                    <ReactQueryDevtools initialIsOpen={false} />
-                    <BrowserRouter>
-                        <Suspense fallback={<LoaderPage />}>
-                            <Routes>
-                                {routes}
-                            </Routes>
-                        </Suspense>
-                    </BrowserRouter>
-
-                    {/* Toaster Configrations  */}
-                    <Toaster
-                        position="top-center"
-                        gutter={12}
-                        containerStyle={{ margin: "8px" }}
-                        toastOptions={{
-                            success: {
-                                duration: 3000,
-                            },
-                            error: {
-                                duration: 5000,
-                            },
-                            style: {
-                                fontSize: "16px",
-                                maxWidth: "500px",
-                                padding: "16px 24px",
-                                backgroundColor: "white",
-                                color: "black",
-                            },
-                        }}
-                    />
-                </QueryClientProvider>
-            </NextUIProvider>
-        </ThemeProvider>
-    );
+          {/* Toaster Configrations  */}
+          <Toaster
+            position="top-center"
+            gutter={12}
+            containerStyle={{ margin: "8px" }}
+            toastOptions={{
+              success: {
+                duration: 3000,
+              },
+              error: {
+                duration: 5000,
+              },
+              style: {
+                fontSize: "16px",
+                maxWidth: "500px",
+                padding: "16px 24px",
+                backgroundColor: "white",
+                color: "black",
+              },
+            }}
+          />
+        </QueryClientProvider>
+      </NextUIProvider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
