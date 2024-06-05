87419f44fbd8b96e5e500bf4ffa90700e18406be
ahmed <ahmedosama12313@gmail.com>
2024-06-06 00:19:14 +0300

    add profile card to  sidebar

--- src/layouts/campusSuit/components/Layout.jsx
+++ src/layouts/campusSuit/components/Layout.jsx
@@ -12,10 +12,13 @@ import ListItem from "@mui/material/ListItem";
 import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 
+import { STORAGE_LINK } from "@/utils/constants";
+import { IoLogOut } from "react-icons/io5";
+
 import UserDropdown from "@/components/UserDropdown";
 import Logo from "@/components/Logo";
 import { useAuth } from "../../../hooks/auth/useAuth";
-
+import { Skeleton, User } from "@nextui-org/react";
 
 const drawerWidth = 300;
 
@@ -23,7 +26,7 @@ function Layout({ children, sidebarLinks }) {
   const [mobileOpen, setMobileOpen] = useState(false);
   const [isClosing, setIsClosing] = useState(false);
 
-  const { useUser } = useAuth({ gardName: 'admin' })
+  const { useUser } = useAuth({ gardName: "admin" });
 
   const { isAuthenticated, isPending, user } = useUser();
 
@@ -43,28 +46,57 @@ function Layout({ children, sidebarLinks }) {
   };
 
   const drawer = (
-    <div className="flex flex-col items-center h-full gap-10 py-6 bg-blue-color-primary">
-      <Link to="/">
-        <Logo logo="/images/logo/logoWhite.svg" width="120" />
-      </Link>
+    <>
+      <div className="flex flex-col items-center h-full gap-10 py-6 bg-blue-color-primary">
+        <div className="flex flex-col items-center w-full h-full gap-10 py-6 bg-blue-color-primary">
+          <Link to="/">
+            <Logo logo="/images/logo/logoWhite.svg" width="120" />
+          </Link>
 
-      <List className="w-full">
-        {sidebarLinks?.map((item, index) => (
-          <ListItem key={index} sx={{ "&.MuiListItem-root": { justifyContent: "center", transition: "all 2s" } }}>
-            <NavLink
-              to={item.to}
-              className={({ isActive }) =>
-                `flex items-center duration-300 py-4  w-11/12 gap-x-2 pl-6 text-white
+          <List className="w-full">
+            {sidebarLinks?.map((item, index) => (
+              <ListItem key={index} sx={{ "&.MuiListItem-root": { justifyContent: "center", transition: "all 2s" } }}>
+                <NavLink
+                  to={item.to}
+                  className={({ isActive }) =>
+                    `flex items-center duration-300 py-4  w-11/12 gap-x-2 pl-6 text-white
                                 ${isActive && `font-bold bg-blue-color-light rounded-md`}`
-              }
-            >
-              <span className="duration-300">{item.icon}</span>
-              <p className="capitalize text-md ">{item.name}</p>
+                  }
+                >
+                  <span className="duration-300">{item.icon}</span>
+                  <p className="capitalize text-md ">{item.name}</p>
+                </NavLink>
+              </ListItem>
+            ))}
+          </List>
+        </div>
+        {isPending ? (
+          <div className="max-w-[250px] w-full flex justify-center items-center gap-3">
+            <div>
+              <Skeleton className="flex w-12 h-12 rounded-full" />
+            </div>
+            <div className="flex flex-col w-full gap-2">
+              <Skeleton className="w-3/5 h-3 rounded-lg" />
+              <Skeleton className="w-4/5 h-3 rounded-lg" />
+            </div>
+          </div>
+        ) : (
+          <div className="flex items-center justify-between w-11/12 px-8 py-2 bg-white rounded-lg ">
+            <NavLink to={"item.to"} className={({ isActive }) => `${isActive && `font-bold bg-blue-color-light rounded-md text-white`}`} size="sm" color="white" isExternal>
+              <User
+                className="font-bold"
+                name={user?.attributes?.name}
+                description={user?.attributes?.email}
+                avatarProps={{
+                  src: user?.attributes?.image === null ? "/images/userPlaceholder.png" : `${STORAGE_LINK}/${user?.attributes?.image}`,
+                }}
+              />
             </NavLink>
-          </ListItem>
-        ))}
-      </List>
-    </div>
+            <IoLogOut className="text-3xl" />
+          </div>
+        )}
+      </div>
+    </>
   );
 
   return (
@@ -84,8 +116,7 @@ function Layout({ children, sidebarLinks }) {
           <IconButton color="inherit" aria-label="open drawer" edge="start" onClick={handleDrawerToggle} sx={{ mr: 2, display: { md: "none" }, color: "#4E74F9" }}>
             <MenuIcon />
           </IconButton>
-          <div className="flex justify-end w-full py-3 ">{isAuthenticated && !isPending && <UserDropdown logoutRedirect='/admin/login'
-            user={user} gardName='admin' admin={true} />}</div>
+          <div className="flex justify-end w-full py-3 ">{isAuthenticated && !isPending && <UserDropdown logoutRedirect="/admin/login" user={user} gardName="admin" admin={true} />}</div>
         </Toolbar>
       </AppBar>
       <Box component="nav" sx={{ flexShrink: { xs: 1 } }} aria-label="mailbox folders">
