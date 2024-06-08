74033892947dbd18a6b7fd4fc8fd5f13bd507405
ahmed <ahmedosama12313@gmail.com>
2024-06-08 06:03:25 +0300

    fix styles adding some height and width and make limites on inputs

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,3 +5,8 @@ export const API_ADMIN = import.meta.env.VITE_API_ADMIN;
 export const API_WEB = import.meta.env.VITE_API_WEB;
 
 export const STORAGE_LINK = import.meta.env.VITE_STORAGE;
+
+//constants input limits
+
+export const UNI_NAME = 50;
+export const UNI_DESCRIPTION = 55;
